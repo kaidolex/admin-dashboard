3fffa29e5c47f0d8c9349f65924bfb61f9055de4
Kaidolex <kaidolex@praganoid.com>
2025-09-08 21:14:58 +0800

    added tailwind and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  tailwindcss: {
+    exposeConfig: true,
+    viewer: { endpoint: "/_tailwind", exportViewer: true },
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
