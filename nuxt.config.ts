a26f565e036fda55054777c876df28ffaafd65b5
Kaidolex <kaidolex@praganoid.com>
2025-09-08 22:17:09 +0800

    fix shadcn and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
-  tailwindcss: {
-    exposeConfig: true,
-    viewer: { endpoint: "/_tailwind", exportViewer: true },
-  },
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "shadcn-nuxt"],
   shadcn: {
     prefix: "",
     componentDir: "./components/ui",
+  },
+
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
