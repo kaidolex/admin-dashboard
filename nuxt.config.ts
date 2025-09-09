54cb61ab8dc35b8b6492d44c3ac3a803f0e1852d
Kaidolex <kaidolex@praganoid.com>
2025-09-09 20:59:02 +0800

    reverted to base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils", "shadcn-nuxt"],
-  shadcn: {
-    prefix: "",
-    componentDir: "./components/ui",
-  },
-
-  css: ["~/assets/css/tailwind.css"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/scripts", "@nuxt/test-utils"],
 });
